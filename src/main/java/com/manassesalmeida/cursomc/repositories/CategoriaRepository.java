c0cc2dd0937d7228804cb49d5747ac6119b57d7f
Manasses Almeida <junior.jta26@hotmail.com>
2018-05-24 15:05:48 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/manassesalmeida/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.manassesalmeida.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.manassesalmeida.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
