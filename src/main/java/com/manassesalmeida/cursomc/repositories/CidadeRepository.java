0ef90466674ccaae170b5dd809f350b2b58dd64c
Manasses Almeida <junior.jta26@hotmail.com>
2018-05-25 12:04:40 -0300

    Estado e Cidade

---
+++ src/main/java/com/manassesalmeida/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.manassesalmeida.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.manassesalmeida.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
