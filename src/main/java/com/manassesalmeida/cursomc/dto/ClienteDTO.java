f2ffab1ade082c8da543a2ed84a6aa56725d91c3
Manasses Almeida <junior.jta26@hotmail.com>
2018-06-08 00:33:42 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/manassesalmeida/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/manassesalmeida/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.manassesalmeida.cursomc.domain.Cliente;
+import com.manassesalmeida.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	/**
