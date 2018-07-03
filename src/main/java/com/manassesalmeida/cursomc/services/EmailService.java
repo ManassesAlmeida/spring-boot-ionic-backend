2f9251d8ca1311527f090818b4870fcc9de115bc
Manasses Almeida <junior.jta26@hotmail.com>
2018-07-03 01:31:10 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

---
+++ src/main/java/com/manassesalmeida/cursomc/services/EmailService.java
@@ -0,0 +1,14 @@
+package com.manassesalmeida.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.manassesalmeida.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+	
+	
+}
